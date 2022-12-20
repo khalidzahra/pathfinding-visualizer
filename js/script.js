9ad9194e67768da6a8c1a7cb0daa882777cc0cec
khalidzahra <khalidzahra923@gmail.com>
2022-12-20 16:04:54 +0200

    Add more detailed comments to the code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*
-     KEY
+    KEY -> used for node type identification 
     0 - unvisited
     1 - visited
     2 - start node
@@ -9,15 +9,16 @@
     6 - location
 */
 
+// Defines the total number of nodes in the grid, and calculates number of rows for initializing the nodes array.
 const NUMBER_OF_NODES = 1000, NODES_PER_ROW = 50, NUMBER_OF_ROWS = NUMBER_OF_NODES / NODES_PER_ROW;
-const TABLE_QUERY = '#grid-container table';
-var start_node, end_node, pathLocations;
-var isReset = false, isDragging = false, hasStarted = false, isInEndPhase = false;
+const TABLE_QUERY = '#grid-container table'; // Used to select the grid using its css ID.
+var start_node, end_node, pathLocations; // Stores the start node, end node, and locations that the path must pass through.
+var isReset = false, isDragging = false, hasStarted = false, isInEndPhase = false; // Flags for identifying different actions and current project phase.
 
-// declaring nodes array
+// Declaring nodes array
 var nodes;
 
-// initializing all variables
+// Initializing all variables
 function init() {
     nodes = Array.from({ length: NUMBER_OF_ROWS }, () => 
         Array.from({ length: NODES_PER_ROW }, () => 0)
@@ -29,31 +30,31 @@ function init() {
     nodes[end_node[0]][end_node[1]] = 3; // end node
 }
 
-// handles a node click event
+// Handles a node click event
 function nodeClickHandler(e) {
     if (!e.target.classList.contains("blocked-node")) {
         if (isDragging || hasStarted)
             return;
         let row = e.target.parentNode.rowIndex;
         let col = e.target.cellIndex;
-        if (nodes[row][col] !== 0)
+        if (nodes[row][col] !== 0) // If the clicked node is not a normal unvisited node, then ignore.
             return;
-        if (e.shiftKey) {
+        if (e.shiftKey) { // If it was a shift-click, then set the node as a path location.
             nodes[row][col] = 6;
             e.target.classList.add("location-node");
             pathLocations.push([row, col]);
-        } else {
+        } else { // If it was a normal click, then set the node as a blocked node.
             nodes[row][col] = 5;
             e.target.classList.add("blocked-node");
         }
     } 
 }
 
-// adds required listeners to support draggable nodes
+// Adds required listeners to support draggable nodes
 function addDragListeners() {
-    // listeners for each cell
+    // Listeners for each cell
     document.querySelectorAll("td").forEach(cell => {
-        // the beginning of dragging
+        // Start of dragging
         cell.addEventListener("dragstart", e => {
             if (!hasStarted && cell.draggable && !isInEndPhase) {
                 isDragging = true;
@@ -61,20 +62,21 @@ function addDragListeners() {
                 e.dataTransfer.setData("boolean", true);
             }
         });
-        // when dragging over the cell
+        // Highlights node currently being dragged over
         cell.addEventListener("dragover", e => {
             e.preventDefault();
             if (e.dataTransfer.getData("boolean") && !isInEndPhase && !hasStarted)
                 cell.classList.add("drag-over");
         });
-        // when cursor leaves the cell while dragging
+        // Removes cell highlighting when dragged node leaves the cell
         cell.addEventListener("dragleave", e => removeClassFromCell(cell.parentNode.rowIndex, cell.cellIndex, "drag-over"));
-        // when dropping the image into the cell
+        // Handles dropping the node in its final location.
         cell.addEventListener("drop", e => {
             e.preventDefault();
             let isNode = e.dataTransfer.getData("boolean");
             let data = e.dataTransfer.getData("text/plain");
-            if (isNode && data != null && !e.target.classList.contains("blocked-node") && !hasStarted && !isInEndPhase) {
+            if (isNode && data != null && !e.target.classList.contains("blocked-node") && !hasStarted && !isInEndPhase) { // Checks if dropped node and target node are both valid
+                // Checks which node was dragged (start/end) and updates their locations in the nodes array.
                 if (data === "start-node") {
                     removeClassFromCell(start_node[0], start_node[1], data);
                     setCellDraggable(start_node[0], start_node[1], false);
@@ -88,23 +90,25 @@ function addDragListeners() {
                     end_node = [cell.parentNode.rowIndex, cell.cellIndex];
                     nodes[end_node[0]][end_node[1]] = 3;
                 }
+                // Transfers the CSS class to the new node and allows it to be dragged.
                 cell.classList.add(data);
                 setCellDraggable(cell, true);
             }
-            removeClassFromCell(cell.parentNode.rowIndex, cell.cellIndex, "drag-over");
+            removeClassFromCell(cell.parentNode.rowIndex, cell.cellIndex, "drag-over"); // Removes the dragover class after dropping the node.
             isDragging = false;
         });
     });
 
 }
 
-// grid generation
+// Grid generation
 function generateGrid(table) {
     for (let i = 0; i < NUMBER_OF_ROWS; i++) {
         let row = table.insertRow();
         row.draggable = false;
         for (let j = 0; j < NODES_PER_ROW; j++) {
-            let cell = row.insertCell();   
+            let cell = row.insertCell();
+            // Checks if current node is start/end node and allows it to be draggable.   
             if (nodes[i][j] == 2) {
                 cell.classList.add("start-node");
                 setCellDraggable(cell, true);
@@ -112,21 +116,23 @@ function generateGrid(table) {
                 cell.classList.add("end-node");
                 setCellDraggable(cell, true);
             }
-            cell.addEventListener("click", e => nodeClickHandler(e))
-            cell.addEventListener("mouseover", e => {
+            cell.addEventListener("click", e => nodeClickHandler(e)); // Adds the click handler to each cell in the grid.
+            cell.addEventListener("mouseover", e => { // Does the same but this is for supporting holding down mouse button.
                 if (e.buttons == 1)
                     nodeClickHandler(e);
             });
         }
     }
+    // Adds drag listeners to all cells.
     addDragListeners();
 }
 
-// reset the grid
+// Resets the grid and clears system variables.
 function reset() {
     isReset = true;
     isInEndPhase = false;
     let cells = document.getElementById("grid-container").getElementsByTagName("td");
+    // Clears classes given to all cells.
     for (let cell of cells) {
         cell.classList = [];
     }
@@ -135,6 +141,7 @@ function reset() {
     addClassToCell(end_node[0], end_node[1], "end-node");
 }
 
+// Does a soft reset by only clearing cell classes. Used when pathing from a new path location.
 function softReset() {
     let cells = document.getElementById("grid-container").getElementsByTagName("td");
     for (let cell of cells) {
@@ -146,11 +153,12 @@ function softReset() {
     }
 }
 
+// Checks if the given cell is traversable.
 function canBeTraveresed(row, col) {
     return !(nodes[row][col] == 1 || nodes[row][col] == 4 || nodes[row][col] == 5);
 }
 
-// returns node's immediate neighbours
+// Returns given node's valid and traversable immediate neighbours.
 function getNeighbours(row, col) {
     neighbours = [];
     if (row - 1 > -1 && canBeTraveresed(row - 1, col)) 
@@ -164,34 +172,39 @@ function getNeighbours(row, col) {
     return neighbours;
 }
 
+// Returns the DOM object of the given cell.
 function getCell(row, col) {
     return document.querySelector(TABLE_QUERY).rows[row].cells[col];
 }
 
+// Removes the given class from the specified cell.
 function removeClassFromCell(row, col, clazz) {
     let cell = document.querySelector(TABLE_QUERY).rows[row].cells[col];
     cell.className = cell.className.replace(new RegExp('(?:^|\\s)'+ clazz + '(?:\\s|$)'), ' ');
 }
 
+// Adds the given class to the specified cell.
 function addClassToCell(row, col, clazz) {
     document.querySelector(TABLE_QUERY).rows[row].cells[col].classList.add(clazz);
 }
 
+// Sets the draggable property of a cell to the specified value (boolean).
 function setCellDraggable(cell, draggable) {
     cell.draggable = draggable;
 }
 
-// marks node as visited
+// Marks specified node as visited.
 function setVisited(row, col) {
     nodes[row][col] = 1;
     addClassToCell(row, col, "visited");
 }
 
+// Creates a timeout for the specified amount of time.
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// selects cost function to use
+// Returns the cost calculated according to the cost function specified.
 function calculateCost(node, f) {
     if (f == "cost") {
         return node.cost;
@@ -202,7 +215,7 @@ function calculateCost(node, f) {
     }
 }
 
-// priority queue implementation
+// Priority queue implementation. Takes the cost function to sort by.
 function PriorityQueue(prop) {
     var arr = [];
     this.push = function (el) {
@@ -237,7 +250,7 @@ function PriorityQueue(prop) {
     };
 }
 
-// sets the parent and the cost of the specified node
+// Sets the parent and the cost of the specified node
 function setParentCost(path, node, _parent, _cost) {
     path[node] = {
         parent: _parent,
@@ -245,62 +258,74 @@ function setParentCost(path, node, _parent, _cost) {
     }
 }
 
-// finds the shortest path to the end node
+// Finds the shortest path to the end node
 async function findEndNode(startRow, startCol, finalPath) {
+    // Sets system flags.
     if (isReset)
         isReset = false;
     hasStarted = true;
+
     let path = {};
     path[`${startRow},${startCol}`] = {
         parent: null,
         cost: 1,
         heuristic: Math.sqrt(Math.pow(startRow - end_node[0], 2) + Math.pow(startCol - end_node[1], 2))
     };
+
     let queue = new PriorityQueue(document.getElementById("algorithm-selector").value);
     queue.push({
         pos: [startRow, startCol],
         cost: path[`${startRow},${startCol}`].cost,
         heuristic: path[`${startRow},${startCol}`].heuristic
     });
+
+    // Sets the current target node to be found. Sets it to the end node if no path locations are present.
     let targetNode;
     if (pathLocations.length > 0) {
         targetNode = pathLocations.shift();
     } else {
         targetNode = end_node;
     }
+
     while (queue.length() > 0) {
-        await timeout(10);
-        if (isReset)
+        await timeout(10); // Timeout for animation.
+        if (isReset) // Stops if user resets mid-way through.
             break;
-        let node = queue.pop();
-        node = node.pos;
-        if (node[0] == targetNode[0] && node[1] == targetNode[1]) {
-            finalPath = finalPath.concat(getPathBetween(path, [startRow, startCol], targetNode));
-            if (targetNode != end_node) {
+        let node = queue.pop(); // Pops node at the top of the frontier.
+        node = node.pos; // The node returned from the frontier is an object with cost and heuristic but we only need its position now.
+        if (node[0] == targetNode[0] && node[1] == targetNode[1]) { // If the target node was reached then add the path to the final path and check if there are more target nodes (path locations/end node).
+            finalPath = finalPath.concat(getPathBetween(path, [startRow, startCol], targetNode)); // Adds the path between the start node and the target node to the final path.
+            if (targetNode != end_node) { // If the target node is not the end node then start again and find the end node.
                 softReset();
                 findEndNode(targetNode[0], targetNode[1], finalPath);
-            } else {
+            } else { // Otherwise, draw the final path for the user.
                 softReset();
                 drawPath(finalPath);
             }
             break;
         }
+        // Sets current node to visited.
         setVisited(node[0], node[1]);
+        // Finds neighbours, calculates their costs, and adds them to the queue.
         handleNeighbours(node, queue, path, targetNode);
     }
+
     hasStarted = false;
 }
 
-// adds neighbours to queue to be processed and calculates their costs
+// Adds neighbours to queue to be processed and calculates their costs.
 function handleNeighbours(node, queue, path, targetNode) {
     getNeighbours(node[0], node[1]).forEach(n => {
+        // Pushes node object containing cost and heuristic to the queue.
         queue.push({
             pos: [n[0], n[1]],
             cost: 1,
             heuristic: Math.sqrt(Math.pow(n[0] - targetNode[0], 2) + Math.pow(n[1] - targetNode[1], 2))
         });
+        // Sets node type to queued.
         nodes[n[0]][n[1]] = 4;
         addClassToCell(n[0], n[1], "queued");
+        // Sets parent and cost of the node.
         let nodeKey = `${n[0]},${n[1]}`;
         let parentKey = `${node[0]},${node[1]}`;
         if (!path.hasOwnProperty(nodeKey) || path[parentKey].cost + 1 < path[nodeKey].cost)
@@ -308,7 +333,9 @@ function handleNeighbours(node, queue, path, targetNode) {
     });
 }
 
+// Finds the nodes creating the path between the two specified nodes.
 function getPathBetween(path, start, end) {
+    // Starts from the end node and goes up to each parent until it finds the start node.
     let curr = `${end[0]},${end[1]}`;
     let finalPath = [];
     while (curr != `${start[0]},${start[1]}`) {
@@ -316,34 +343,41 @@ function getPathBetween(path, start, end) {
         finalPath.push([parseInt(info[0]), parseInt(info[1])]);
         curr = path[curr].parent;
     }
+    // Adds the start node because the loop terminates before it is added.
     finalPath.push([start[0], start[1]]);
-    return finalPath.reverse();
+    return finalPath.reverse(); // Returns the reversed path so that it is in-order (start to end).
 }
 
-// draws the final shortest path between the start and end nodes
+// Draws the final shortest path between the start and end nodes.
 async function drawPath(path) {
     while (path.length > 0) {
-        await timeout(5);
-        let currNode = path.shift();
-        addClassToCell(currNode[0], currNode[1], "path");
+        await timeout(5); // Timeout for animation.
+        let currNode = path.shift(); // Finds the node at the top of the queue.
+        addClassToCell(currNode[0], currNode[1], "path"); // Colors the node to indicate that it is part of the final path.
     }
-    isInEndPhase = true;
+    isInEndPhase = true; // Sets system flag.
 }
 
+// Initializes system variables.
 init();
+// Generates the HTML tables that forms the grid.
 generateGrid(document.querySelector(TABLE_QUERY));
 
+// Event listener for the start button.
 document.querySelector("#start-button").addEventListener("click", () => {
-    if (isInEndPhase)
+    if (isInEndPhase) // If visualization had already ended, then reset the grid and the system variables.
         reset();
+    // Sort the path locations according to their distance from the start node.
     pathLocations.sort(function(a,b) {
         var firstDist = Math.pow((start_node[1] - a[1]), 2) + Math.pow((start_node[0] - a[0]), 2);
         var secDist = Math.pow((start_node[1] - b[1]), 2) + Math.pow((start_node[0] - b[0]), 2);
         return firstDist - secDist;
     });
+    // Find the shortest path.
     findEndNode(start_node[0], start_node[1], []);
 });
 
+// Event listener for the reset button.
 document.querySelector("#reset-button").addEventListener("click", () => {
     reset();
 });
